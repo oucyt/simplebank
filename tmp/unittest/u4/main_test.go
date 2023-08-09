f3ab34c80a3b19573b830260419b021896b13fc9
白炽灯 <876271327@qq.com>
2023-08-09 03:48:58 -0700

    update

--- tmp/unittest/u4/main_test.go
+++ tmp/unittest/u4/main_test.go
@@ -1,13 +1,12 @@
 package u4
 
 import (
-	"fmt"
 	"os"
 	"testing"
 )
 
+// [Go语言标准库文档中文版 | Go语言中文网 | Golang中文社区 | Golang中国 (studygolang.com)](https://studygolang.com/pkgdoc)
 func TestMain(m *testing.M) {
-	fmt.Println("invoke TestMain")
 	// call flag.Parse() here if TestMain uses flags
 	// 如果 TestMain 使用了 flags，这里应该加上 flag.Parse()
 	os.Exit(m.Run())
